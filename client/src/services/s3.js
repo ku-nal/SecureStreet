ec9ce3f44a84c28556d8bccbbd47b343f8c2e451
kunal <kn362288@dal.ca>
2024-04-09 22:39:27 +0530

    Deleted file

--- client/src/services/s3.js
+++
@@ -1,9 +0,0 @@
-import AWS from 'aws-sdk';
-
-const S3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  });
-
-
-export default S3;
