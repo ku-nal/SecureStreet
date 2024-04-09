47cb75c07fb375e1758f3f3003b2e24dd02f8afb
kunal <kn362288@dal.ca>
2024-04-09 21:41:44 +0530

    .env

--- client/src/services/s3.js
+++ client/src/services/s3.js
@@ -1,8 +1,8 @@
 import AWS from 'aws-sdk';
 
 const S3 = new AWS.S3({
-    accessKeyId: "AKIA6ODUZGZGAC2KVNJG",
-    secretAccessKey: "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"
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
   });
 
 
